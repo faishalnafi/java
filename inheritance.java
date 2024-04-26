af3062ccb4eb5dac5704d17683e767146c17b59a
faishalnafi <faishalnafi50@gmail.com>
2024-04-26 19:45:20 +0700

    inheritance

---
+++ inheritance.java
@@ -0,0 +1,12 @@
+//Mata Kuliah Pemrograman Berbasis Objek;
+//Dibuat 26 April 2024 FaishalNafi';
+
+class ras {
+    String kulit, tinggi, bahasa, benuaras;
+    int id;
+    double jumlah;
+}
+
+public class inheritance {
+    //code
+}
