b7e40d4e61fb7556634979df00a37396d97276d4
faishalnafi <faishalnafi50@gmail.com>
2024-06-28 10:18:31 +0700

    gui

---
+++ GUI/main.java
@@ -0,0 +1,10 @@
+// program GUI mata kuliah Pratikum PBO
+// 28 juni 2024
+// fasihal nafi 13.2023.1.01210
+package GUI;
+
+public class main {
+    public static void main(String[] args) {
+        
+    }
+}
