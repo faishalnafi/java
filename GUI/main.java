a8811730668f0e6f981e36b892d17f320027d345
faishalnafi <faishalnafi50@gmail.com>
2025-01-05 09:52:26 +0700

    penataan folder

--- GUI/main.java
+++
@@ -1,9 +0,0 @@
-// program GUI mata kuliah Pratikum PBO
-// 28 juni 2024
-// fasihal nafi' 13.2023.1.01210
-
-public class main {
-    public static void main(String[] args) {
-        System.out.println("halo dunia");
-    }
-}
