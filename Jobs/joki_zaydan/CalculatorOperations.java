737d7a43bb17275a0373c505b47523cc0dc954e4
faishalnafi <faishalnafi50@gmail.com>
2025-01-10 19:44:01 +0700

    Add bug

--- Jobs/joki_zaydan/CalculatorOperations.java
+++ Jobs/joki_zaydan/CalculatorOperations.java
@@ -2,6 +2,6 @@ package joki_zaydan;
 
 public class CalculatorOperations {
     public double evaluate(String expression) {
-        return new ExpressionParser().parse(expression);
+        return new ExpressionParser();
     }
 }
