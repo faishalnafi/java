091cb288f0f2b823f1f2541776d747f15c895960
faishalnafi <faishalnafi50@gmail.com>
2025-01-10 19:42:45 +0700

    Add bug

--- Jobs/joki_zaydan/Main.java
+++ Jobs/joki_zaydan/Main.java
@@ -4,10 +4,8 @@
 
 package joki_zaydan;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(CalculatorUI::new);
+
     }
 }
